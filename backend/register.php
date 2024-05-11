3bb9eba2eafe40e7765d164d3111c3d0d5d621fe
BenjaminCoach <98662160+BenjaminLoggenberg@users.noreply.github.com>
2024-05-11 11:17:20 +0200

    create and receive user data and registation

---
+++ backend/register.php
@@ -0,0 +1,26 @@
+<?php
+// register.php
+include_once 'db.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+$username = $data->username;
+$password = password_hash($data->password, PASSWORD_BCRYPT);
+
+$db = new Database();
+$conn = $db->getConnection();
+
+$query = "INSERT INTO users SET username = :username, password = :password";
+
+$stmt = $conn->prepare($query);
+
+$stmt->bindParam(":username", $username);
+$stmt->bindParam(":password", $password);
+
+if($stmt->execute()) {
+    echo json_encode(array("message" => "User was created."));
+} else {
+    echo json_encode(array("message" => "Unable to create user."));
+}
+?>
+
